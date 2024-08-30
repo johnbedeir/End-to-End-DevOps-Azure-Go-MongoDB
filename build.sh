c8d69496f06dc67565272688f4beabfa7b820346
jnb <jnb@e-boks.dk>
2024-08-30 11:35:23 +0200

    using CircleCI Pipeline

--- build.sh
+++ build.sh
@@ -6,9 +6,8 @@ cluster_name="cluster-1-dev-aks" # Update this if the cluster name is changed in
 Location="Germany West Central"
 resource_group="cluster-1-dev-rg"
 acr_name="cluster1devacr"
-service_perincipal_name="goapp"
+service_principal_name="goapp"
 image_name="$acr_name.azurecr.io/goapp:latest"
-AKS_MANAGED_IDENTITY=$(az aks show --resource-group $resource_group --name $cluster_name --query "identityProfile.kubeletidentity.clientId" -o tsv)
 service_name="goapp"
 argo_service_name="argocd-server"
 alertmanager_service_name="kube-prometheus-stack-alertmanager"
@@ -17,58 +16,46 @@ prometheus_service_name="kube-prometheus-stack-prometheus"
 namespace="go-survey" # Update this if the namespace is changed in k8s manifests
 argo_namespace="argocd"
 monitoring_namespace="monitoring"
-argo_pass=$(kubectl -n $argo_namespace get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
-grafana_user=$(kubectl -n $monitoring_namespace get secret $grafana_service_name -o jsonpath="{.data.admin-user}" | base64 --decode)
-grafana_pass=$(kubectl -n $monitoring_namespace get secret $grafana_service_name -o jsonpath="{.data.admin-password}" | base64 --decode)
 app_port="8080"
 alertmanager_port="9093"
 prometheus_port="9090"
 # End of Variables
 
 # Check if the service principal already exists
-sp_exists=$(az ad sp list --display-name $service_perincipal_name --query "[?appDisplayName=='$service_perincipal_name'].appId" -o tsv)
+echo "------------Check if Service Principal Exists---------------"
+service_principal_id=$(az ad sp list --display-name "$service_principal_name" --query "[?appDisplayName=='$service_principal_name'].appId" -o tsv)
 
-# If the service principal doesn't exist, create it
-if [ -z "$sp_exists" ]; then
-    echo "Service principal does not exist. Creating a new one..."
-    service_perincipal=$(az ad sp create-for-rbac --name "$service_perincipal_name" --role Contributor --scopes /subscriptions/$subscription_id)
-
-    # Extract details
-    CLIENT_ID=$(echo $service_perincipal | jq -r .appId)
-    CLIENT_SECRET=$(echo $service_perincipal | jq -r .password)
-    TENANT_ID=$(echo $service_perincipal | jq -r .tenant)
-
-    echo "Client ID: $CLIENT_ID"
-    echo "Client Secret: $CLIENT_SECRET"
-    echo "Tenant ID: $TENANT_ID"
+if [ -z "$service_principal_id" ]; then
+  echo "Service principal '$service_principal_name' does not exist, Run the script 'run_me_first.sh' to create one."
+  exit 1
 else
-    echo "Service principal already exists with App ID: $sp_exists"
+  echo "Service principal '$service_principal_name' exists with ID: $service_principal_id"
 fi
 
 # Update helm repos
 helm repo update
 
 # Build the infrastructure
-echo "--------------------Creating AKS & ACR--------------------"
+echo "---------------------Creating AKS & ACR---------------------"
 cd terraform || { echo "Terraform directory not found"; exit 1; }
 terraform init || { echo "Terraform init failed"; exit 1; }
 terraform apply -auto-approve || { echo "Terraform apply failed"; exit 1; }
 cd ..
 
 # Update kubeconfig
-echo "--------------------Updating Kubeconfig--------------------"
+echo "--------------------Updating Kubeconfig---------------------"
 az aks get-credentials --resource-group $resource_group --name $cluster_name || { echo "Failed to update kubeconfig"; exit 1; }
 
 # Remove previous Docker images
-echo "--------------------Removing Previous Build--------------------"
+echo "------------------Removing Previous Build-------------------"
 docker rmi -f $image_name || true
 
 # Build new Docker image with new tag
-echo "--------------------Building New Image--------------------"
-docker build -t $image_name ./Go-app/ || { echo "Docker build failed"; exit 1; }
+echo "---------------------Building New Image---------------------"
+docker build --platform linux/amd64 -t $image_name ./Go-app/ || { echo "Docker build failed"; exit 1; }
 
 # ACR Login
-echo "--------------------Logging into ACR--------------------"
+echo "----------------------Logging into ACR----------------------"
 az acr login --name $acr_name || { echo "ACR login failed"; exit 1; }
 
 # Push the latest build to ACR
@@ -76,13 +63,15 @@ echo "--------------------Pushing Docker Image--------------------"
 docker push $image_name || { echo "Docker push failed"; exit 1; }
 
 # Create namespace
-echo "--------------------Creating Namespace--------------------"
+echo "---------------------Creating Namespace---------------------"
 kubectl create ns $namespace || true
 
+# Create role assignment
+AKS_MANAGED_IDENTITY=$(az aks show --resource-group $resource_group --name $cluster_name --query "identityProfile.kubeletidentity.clientId" -o tsv)
 az role assignment create --assignee $AKS_MANAGED_IDENTITY --scope $(az acr show --name $acr_name --resource-group $resource_group --query id -o tsv) --role AcrPull
 
 # Deploy the application
-echo "--------------------Deploying App--------------------"
+echo "-----------------------Deploying App------------------------"
 kubectl apply -n $namespace -f k8s || { echo "App deployment failed"; exit 1; }
 
 # Wait for application pods to be running
@@ -90,18 +79,18 @@ echo "--------------------Waiting for App Pods to be Running--------------------
 kubectl wait --for=condition=ready pod -l app=$service_name -n $namespace --timeout=120s || { echo "Pods are not ready"; exit 1; }
 
 # Get LoadBalancer
-echo "--------------------Retrieving App URL--------------------"
-echo "Port=$app_port"
-kubectl get svc $service_name -n $namespace -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || { echo "Failed to retrieve service IP"; exit 1; }
+echo "----------------------Application URL-----------------------"
+echo "$(kubectl get svc $service_name -n $namespace -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || { echo "Failed to retrieve service IP"; exit 1; }):$app_port"
 
 echo ""
 
-echo "--------------------Retrieving ArgoCD URL--------------------"
+echo "------------------------ArgoCD URL--------------------------"
 kubectl get svc $argo_service_name -n $argo_namespace -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || { echo "Failed to retrieve service IP"; exit 1; }
 
 echo ""
 
-echo "-------------------- ArgoCD Credentials--------------------"
+echo "-------------------- ArgoCD Credentials---------------------"
+argo_pass=$(kubectl -n $argo_namespace get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
 echo "┌─────────┬───────────────────────────┐"
 echo "│  USER   │  PASSWORD                 │"
 echo "├─────────┼───────────────────────────┤"
@@ -110,24 +99,24 @@ echo "└─────────┴─────────────�
 
 echo ""
 
-echo "--------------------Retrieving Alertmanager URL--------------------"
-echo "Port=$alertmanager_port"
-kubectl get svc $alertmanager_service_name -n $monitoring_namespace -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || { echo "Failed to retrieve service IP"; exit 1; }
+echo "----------------------Alertmanager URL----------------------"
+echo "$(kubectl get svc $alertmanager_service_name -n $monitoring_namespace -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || { echo "Failed to retrieve service IP"; exit 1; }):$alertmanager_port"
 
 echo ""
 
-echo "--------------------Retrieving Prometheus URL--------------------"
-echo "Port=$prometheus_port"
-kubectl get svc $prometheus_service_name -n $monitoring_namespace -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || { echo "Failed to retrieve service IP"; exit 1; }
+echo "-----------------------Prometheus URL-----------------------"
+echo "$(kubectl get svc $prometheus_service_name -n $monitoring_namespace -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || { echo "Failed to retrieve service IP"; exit 1; }):$prometheus_port"
 
 echo ""
 
-echo "--------------------Retrieving Grafana URL--------------------"
+echo "------------------------ Grafana URL------------------------"
 kubectl get svc $grafana_service_name -n $monitoring_namespace -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || { echo "Failed to retrieve service IP"; exit 1; }
 
 echo ""
 
 echo "-------------------- Grafana Credentials--------------------"
+grafana_user=$(kubectl -n $monitoring_namespace get secret $grafana_service_name -o jsonpath="{.data.admin-user}" | base64 --decode)
+grafana_pass=$(kubectl -n $monitoring_namespace get secret $grafana_service_name -o jsonpath="{.data.admin-password}" | base64 --decode)
 echo "┌───────────────┬─────────────────────────┐"
 echo "│  USER         │  PASSWORD               │"
 echo "├───────────────┼─────────────────────────┤"
